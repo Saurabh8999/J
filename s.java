30cf39a2d03021877a8c138f7ccc14b45a0fc58f
Saurabh Bhatia <38155651+Saurabh8999@users.noreply.github.com>
2021-09-14 17:35:11 +0530

    Update s.java

--- s.java
+++ s.java
@@ -6,5 +6,6 @@ String s2=new String(ch);//converting char array to string
 String s3=new String("example");//creating Java string by new keyword    
 System.out.println(s1);    
 System.out.println(s2);    
-System.out.println(s3);    
+  
+System.out.println(s3+"cdnjxzklj");    
 }} 
